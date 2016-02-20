3231686dfe0768955b713269bbd78708849b95ac
Christian Henry <christian.henry@appdynamics.com>
2016-02-20 15:28:01 -0800

    check in dependencies

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './c9-migration.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-//render(
-//  React.createElement(MainContainer, null),
-//  document.getElementById('root')
-//);
+ReactDOM.render(
+    React.createElement(App, null),
+    document.getElementById('root'));
