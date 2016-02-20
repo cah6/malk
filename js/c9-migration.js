dece33a9bfcf0b6bfd8efa1bf3017afe03195948
Christian Henry <christian.henry@appdynamics.com>
2016-02-20 02:51:02 -0800

    malk multiple videos on each user

--- js/c9-migration.js
+++ js/c9-migration.js
@@ -9,12 +9,12 @@ var MainContainer = React.createClass({
 			'div',
 			{},
 			React.createElement(Sidebar, null),
-			React.createElement(VideoFrame, null)
+			React.createElement(ContentFrame, null)
 			);
 	}
 });
 
-var VideoFrame = React.createClass({
+var ContentFrame = React.createClass({
 	render: function() {
 	  	var style = {
 	  		display: "inline-block",
@@ -22,13 +22,15 @@ var VideoFrame = React.createClass({
 	  		width: "80%",
 	  		backgroundColor: 'black'
 	  	};
+	  	var users = ["Devin", "Jeremy", "Danny", "Christian"]
+	  	var userObjectList = users.map(function(userName) {
+		  	return React.createElement(UsersSharedVideo, {user: userName});
+		});
 	  	return (
 	  		React.createElement(
 	  			'div',
 	  			{style: style},
-	  			React.createElement(SidebarLink, {text: "Browse"}),
-	  			React.createElement(SidebarLink, {text: "Guide"}),
-	  			React.createElement(SidebarLink, {text: "Activity"})
+	  			userObjectList
 	  			)
 	    	);
 		}
@@ -37,28 +39,107 @@ var VideoFrame = React.createClass({
 var UsersSharedVideo = React.createClass({
 	render: function() {
 	  	var style = {
-	  		display: "inline-block",
-	  		leftPadding: "20%",
-	  		width: "80%",
-	  		backgroundColor: 'black'
+	  		paddingLeft: 20,
+		    paddingTop: 20,
+		    paddingBottom: 5,
+		    borderStyle: "solid",
+		    borderTopWidth: 1,
+		    borderBottomWidth: 1,
+		    borderLeftWidth: 0,
+		    borderRightWidth: 0,
+		    borderColor: "#d0d0d0"
 	  	};
 	  	return (
 	  		React.createElement(
 	  			'div',
 	  			{style: style},
-	  			React.createElement(SidebarLink, {text: "Browse"}),
-	  			React.createElement(SidebarLink, {text: "Guide"}),
-	  			React.createElement(SidebarLink, {text: "Activity"})
+	  			React.createElement(UserRecentlySharedLabel, {user: this.props.user}),
+	  			React.createElement(UserRecentlySharedVideoList, null)
 	  			)
 	    	);
 		}
 });
+
+var UserRecentlySharedLabel = React.createClass({
+	render: function() {
+		var style = {
+			color: 'white'
+		}
+	  	return (
+	  		React.createElement(
+	  			'div',
+	  			{style: style},
+	  			this.props.user + " recently shared:"
+	  			)
+	    	);
+		}
+});
+
+var UserRecentlySharedVideoList = React.createClass({
+	render: function() {
+		var style = {
+			color: 'white'
+		};
+		var titlesAndUrl = [
+			{
+				title: "video1",
+				url: "https://www.youtube.com/embed/oYiT-vLjhC4"
+			}, 
+			{
+				title: "video2",
+				url: "https://www.youtube.com/embed/oYiT-vLjhC4"
+			},
+			{
+				title: "video3",
+				url: "https://www.youtube.com/embed/oYiT-vLjhC4"
+			}
+		];
+		var titlesAndUrlObjectList = titlesAndUrl.map(function(titleAndUrlMap) {
+		  	return React.createElement(VideoWithTitle, titleAndUrlMap);
+		});
+	  	return (
+	  		React.createElement(
+	  			'ul',
+	  			{style: style},
+	  			titlesAndUrlObjectList
+	  			)
+	    	);
+		}
+})
+
+var VideoWithTitle = React.createClass({
+	render: function() {
+		var style = {
+			color: 'white'
+		}
+	  	return (
+	  		React.createElement(
+	  			'div',
+	  			{style: style},
+	  			this.props.title,
+	  			React.createElement(EmbeddedVideo, {url: this.props.url})
+	  			)
+	    	);
+		}
+})
+
+var EmbeddedVideo = React.createClass({
+	render: function() {
+	  	return (
+	  		React.createElement(
+	  			'iframe',
+	  			{src: this.props.url}
+	  			)
+	    	);
+		}
+})
 
 var Sidebar = React.createClass({
 
   // using javascript
   render: function() {
   	var style = {
+  		verticalAlign: "top",
   		display: "inline-block",
   		width: "20%",
   		backgroundColor: 'black'
