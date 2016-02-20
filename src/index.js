3135a4623031b7a60f5576f5c87f8d9b6c11c815
Christian Henry <christian.henry@appdynamics.com>
2016-02-20 15:06:51 -0800

    Completely change build system. -Move from CDN -> npm installed packaged -Clean up all unused code -Set up hot loading

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './c9-migration.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+//render(
+//  React.createElement(MainContainer, null),
+//  document.getElementById('root')
+//);
