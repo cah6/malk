a47fb289c978d3023760cd975245e02fa9af0e0b
Christian Henry <christian.henry92@gmail.com>
2016-03-03 12:48:37 -0800

    Change styling over to use purecss

--- src/c9-migration.js
+++ src/c9-migration.js
@@ -1,196 +1,165 @@
 //var Radium = require('radium');
 import React, { Component } from 'react';
+import Radium from 'radium';
+import { div, h, h1, h2, nav, ul, a, iframe, img} from 'react-hyperscript-helpers';
 
 export default class App extends Component {
-	render() {
-		var style = {
-			backgroundColor: 'black'
-		};
-		return React.createElement(
-			'div',
-			{},
-			React.createElement(Sidebar, null),
-			React.createElement(ContentFrame, null)
-		);
-	}
+    render() {
+        return div('#layout .pure-g', {}, [
+                div('.sidebar .pure-u-md-1-5', {}, h(Sidebar)),
+                div('.content .pure-u-md-4-5', {}, h(ContentFrame))
+            ]
+        );
+    }
 }
 
-//var MainContainer = React.createClass({
-//	render: function() {
-//		var style = {
-//			backgroundColor: 'black'
-//		};
-//		return React.createElement(
-//			'div',
-//			{},
-//			React.createElement(Sidebar, null),
-//			React.createElement(ContentFrame, null)
-//			);
-//	}
-//});
-
 var ContentFrame = React.createClass({
-	render: function() {
-	  	var style = {
-	  		display: "inline-block",
-	  		leftPadding: "20%",
-	  		width: "80%",
-	  		backgroundColor: 'black'
-	  	};
-	  	var users = ["Devin", "Jeremy", "Danny", "Christian"]
-	  	var userObjectList = users.map(function(userName) {
-		  	return React.createElement(UsersSharedVideo, {key: Math.random(), user: userName});
-		});
-	  	return (
-	  		React.createElement(
-	  			'div',
-	  			{className: "ContentFrame", style: style},
-	  			userObjectList
-	  			)
-	    	);
-		}
+    render: function () {
+        var users = ["Devin-2", "Jeremy", "Danny", "Christian"]
+        var userObjectList = users.map(function (userName) {
+            return React.createElement(UsersSharedVideo, {key: Math.random(), user: userName});
+        });
+        return (
+            div('.test', {}, [
+                    h1('.content-subhead', ['Now Playing']),
+                    iframe('.pure-u-2-3', {
+                        width: '100%',
+                        height: '400px',
+                        allowFullScreen: true,
+                        src: 'https://www.youtube.com/embed/a2Qnh2OlND8'
+                    }),
+                    div('.pure-u-1-3', 'Area for information'),
+                    div('#the-view', userObjectList)
+                ]
+            )
+        );
+    }
 });
 
 var UsersSharedVideo = React.createClass({
-	render: function() {
-	  	var style = {
-	  		paddingLeft: 20,
-		    paddingTop: 20,
-		    paddingBottom: 5,
-		    borderStyle: "solid",
-		    borderTopWidth: 1,
-		    borderBottomWidth: 1,
-		    borderLeftWidth: 0,
-		    borderRightWidth: 0,
-		    borderColor: "#d0d0d0"
-	  	};
-	  	return (
-	  		React.createElement(
-	  			'div',
-	  			{style: style},
-	  			React.createElement(UserRecentlySharedLabel, {user: this.props.user}),
-	  			React.createElement(UserRecentlySharedVideoList, null)
-	  			)
-	    	);
-		}
+    render: function () {
+        var style = {
+            paddingLeft: 20,
+            paddingTop: 20,
+            paddingBottom: 5,
+            borderStyle: "solid",
+            borderTopWidth: 1,
+            borderBottomWidth: 1,
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderColor: "#d0d0d0"
+        };
+        //return (
+        //    React.createElement(
+        //        'div',
+        //        {style: style},
+        //        React.createElement(UserRecentlySharedLabel, {user: this.props.user}),
+        //        React.createElement(UserRecentlySharedVideoList, null)
+        //    )
+        return div('.friend-feed', [
+                h1('.content-subhead', [this.props.user + ' recently shared:']),
+                img('.pure-u-7-24', {src: 'http://img.youtube.com/vi/a2Qnh2OlND8/0.jpg', style: {height: '230px'}}),
+                img('.pure-u-7-24', {src: 'http://img.youtube.com/vi/a2Qnh2OlND8/0.jpg', style: {height: '230px'}}),
+                img('.pure-u-7-24', {src: 'http://img.youtube.com/vi/a2Qnh2OlND8/0.jpg', style: {height: '230px'}}),
+                a('.pure-button .pure-u-3-24', {}, 'N')
+            ]
+        );
+    }
 });
 
 var UserRecentlySharedLabel = React.createClass({
-	render: function() {
-		var style = {
-			color: 'white'
-		}
-	  	return (
-	  		React.createElement(
-	  			'div',
-	  			{style: style},
-	  			this.props.user + " recently shared:"
-	  			)
-	    	);
-		}
+    render: function () {
+        var style = {
+            color: 'white'
+        }
+        return (
+            React.createElement(
+                'div',
+                {style: style},
+                this.props.user + " recently shared:"
+            )
+        );
+    }
 });
 
 var UserRecentlySharedVideoList = React.createClass({
-	render: function() {
-		var style = {
-			color: 'white'
-		};
-		var titlesAndUrl = [
-			{
-				title: "video1",
-				url: "https://www.youtube.com/embed/oYiT-vLjhC4"
-			}, 
-			{
-				title: "video2",
-				url: "https://www.youtube.com/embed/oYiT-vLjhC4"
-			},
-			{
-				title: "video3",
-				url: "https://www.youtube.com/embed/oYiT-vLjhC4"
-			}
-		];
-		var titlesAndUrlObjectList = titlesAndUrl.map(function(titleAndUrlMap) {
-			titleAndUrlMap["key"] = Math.random();
-		  	return React.createElement(VideoWithTitle, titleAndUrlMap);
-		});
-	  	return (
-	  		React.createElement(
-	  			'ul',
-	  			{style: style},
-	  			titlesAndUrlObjectList
-	  			)
-	    	);
-		}
+    render: function () {
+        var style = {
+            color: 'white'
+        };
+        var titlesAndUrl = [
+            {
+                title: "video1",
+                url: "https://www.youtube.com/embed/oYiT-vLjhC4"
+            },
+            {
+                title: "video2",
+                url: "https://www.youtube.com/embed/oYiT-vLjhC4"
+            },
+            {
+                title: "video3",
+                url: "https://www.youtube.com/embed/oYiT-vLjhC4"
+            }
+        ];
+        var titlesAndUrlObjectList = titlesAndUrl.map(function (titleAndUrlMap) {
+            titleAndUrlMap["key"] = Math.random();
+            return React.createElement(VideoWithTitle, titleAndUrlMap);
+        });
+        return (
+            React.createElement(
+                'ul',
+                {style: style},
+                titlesAndUrlObjectList
+            )
+        );
+    }
 })
 
 var VideoWithTitle = React.createClass({
-	render: function() {
-		var style = {
-			display: "inline-block",
-			width: 300,
-			color: 'white'
-		}
-	  	return (
-	  		React.createElement(
-	  			'div',
-	  			{className: "VideoWithTitle", style: style},
-	  			this.props.title,
-	  			React.createElement(EmbeddedVideo, {url: this.props.url})
-	  			)
-	    	);
-		}
+    render: function () {
+        var style = {
+            display: "inline-block",
+            width: 300,
+            color: 'white'
+        }
+        return (
+            React.createElement(
+                'div',
+                {className: "VideoWithTitle", style: style},
+                this.props.title,
+                React.createElement(EmbeddedVideo, {url: this.props.url})
+            )
+        );
+    }
 })
 
 var EmbeddedVideo = React.createClass({
-	render: function() {
-	  	return (
-	  		React.createElement(
-	  			'iframe',
-	  			{src: "https://www.youtube.com/embed/oYiT-vLjhC4"}
-	  			)
-	    	);
-		}
+    render: function () {
+        return (
+            React.createElement(
+                'iframe',
+                {src: "https://www.youtube.com/embed/oYiT-vLjhC4"}
+            )
+        );
+    }
 })
 
 var Sidebar = React.createClass({
-
-  // using javascript
-  render: function() {
-  	var style = {
-  		verticalAlign: "top",
-  		display: "inline-block",
-  		width: "20%",
-  		backgroundColor: 'black'
-  	};
-  	return (
-  		React.createElement(
-  			'div',
-  			{style: style},
-  			React.createElement(SidebarLink, {text: "Browse"}),
-  			React.createElement(SidebarLink, {text: "Guide"}),
-  			React.createElement(SidebarLink, {text: "Activity"})
-  			)
-    	);
-	}
+    render: function () {
+        return (
+            div('.header',
+                {},
+                //{className: '#layout .header'}
+                [
+                    h1('.brand-title', ['Malk']),
+                    h2('.brand-tagline', ['Subtext']),
+                    nav('.nav', [
+                        ul('.nav-list', [
+                            a('.pure-button', {href: '/'}, 'Login')
+                        ])
+                    ])
+                ]
+            )
+        );
+    }
 });
-
-class SidebarLink extends React.Component {
-	render() {
-		var style = {
-			display: "block",
-    		color: 'white',
-    		textIndent: 20,
-    		lineHeight: "40px",
-    		backgroundColor: 'black',
-    		':hover': {
-      			backgroundColor: 'red'
-    		}
-		};
-   		return React.createElement(
-      		'div',
-      		{style: style},
-      		this.props.text
-    	);
-  	}
-}
-//SidebarLink = Radium(SidebarLink);
